11e4bac467766d202398792ed8c9f998b86e2d94
dautrich <hugo.dautricourt.etu@univ-lille.fr>
2019-09-26 14:51:16 +0200

    Modif : using now buffered reader

--- src/Util/Sauvegarde.java
+++ src/Util/Sauvegarde.java
@@ -2,10 +2,7 @@ package Util;
 
 import Objets.Systeme;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 
 public class Sauvegarde {
     static String path;
@@ -18,8 +15,22 @@ public class Sauvegarde {
         File astroFile = new File(path);
         System.out.print("Le fichier "+astroFile.getName()+" à été chargé.");
         BufferedReader br = new BufferedReader(new FileReader(astroFile));
+        try {
+            String thisLine = br.readLine();
+            while (thisLine != null) {
+                if(thisLine.startsWith("PARAMS")){
+                    String valeur[] = thisLine.split(" ");
+                    String valeur2[] = new String[valeur.length];
+                    for (int i = 2; i < valeur.length ; i++) {
+                        valeur2[i-2] = valeur[i].split("=")[1];
+                    }
+                }
 
+            }
 
+        } catch (IOException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
